5472fd1ad13b49f0af238de4d1469bb09f11bfa0
Kodzu-ann <ann.cooper2023@gmail.com>
2022-10-22 14:55:45 +0200

    installed boostrap

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <h1> Weather app </h1>
       <footer>
         <a
           href="https://github.com/kodzu-ann/react-homework-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           Github link{" "}
         </a>
+        <button className="btn btn-primary"> hi </button>
       </footer>
     </div>
   );
