442d2f5e99b9a23318bce4e4d21f15feb608ba92
Kodzu-ann <ann.cooper2023@gmail.com>
2022-10-22 13:41:06 +0200

    Added Github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1> Weather app </h1>
+      <footer>
+        <a
+          href="https://github.com/kodzu-ann/react-homework-weather-app"
+          target="_blank"
+        >
+          Github link{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
